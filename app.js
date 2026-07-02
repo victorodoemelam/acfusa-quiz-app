eec7fb39725fc6e4de59cfd25521a8944d8f823f
Victor Odoemelam <28530041+victorodoemelam@users.noreply.github.com>
2026-07-02 13:23:21 +0100

    Change access password to 1986

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const QUIZ_PASSWORD='2468';
+const QUIZ_PASSWORD='1986';
 const CATEGORY_NAMES={junior:'JUNIOR',varsity:'VARSITY',bonus:'BONUS ROUND'};
 const screens=[...document.querySelectorAll('.screen')];
 const usedQuestions=new Set();
